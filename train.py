d0a384df4e6bb8897dbed7b2df96c35225cbca23
DRPproton <86702004+DRPproton@users.noreply.github.com>
2023-12-10 15:16:44 -0600

    project done

--- train.py
+++ train.py
@@ -110,3 +110,55 @@ sc_df = sc.fit_transform(X_train)
 X_train = pd.DataFrame(sc_df, columns=X_train.columns)
 X_test = pd.DataFrame(sc.transform(X_test), columns=X_test.columns)
 
+
+print('Creating model...')
+time.sleep(2)
+# XGB model , params got from optuna tuning
+best_parameters = {'booster': 'gbtree', 
+                   'max_depth': 11, 
+                   'learning_rate': 0.04216709720284281, 
+                   'n_estimators': 504, 
+                   'min_child_weight': 1, 
+                   'subsample': 0.3437341948452076, 
+                   'colsample_bylevel': 0.9831950290382536, 
+                   'colsample_bytree': 0.1720049167961935, 
+                   'colsample_bynode': 0.7420031757532206, 
+                   'reg_alpha': 0.9242740053249385, 
+                   'reg_lambda': 0.9419384081768526, 
+                   'eval_metric': 'mlogloss'}
+
+xgb = XGBClassifier(**best_parameters)
+xgb.fit(X_train, y_train)
+xgb_pred = xgb.predict_proba(X_test)
+
+print('Making prediciions...')
+print(f'Logloss score => {log_loss(y_test, xgb_pred)}')
+print(f"roc_auc score = >{roc_auc_score(y_test, xgb_pred, multi_class='ovr')}")
+print(xgb_pred[0])
+
+time.sleep(2)
+
+# Exporting model
+print('Exporting model...')
+import pickle
+
+with open('xgb_model.bin', 'wb') as f_out:
+    pickle.dump(xgb, f_out)
+
+time.sleep(1)
+print('Importing saved model...')
+with open('xgb_model.bin', 'rb') as f_in:
+    model_xgb = pickle.load(f_in)
+
+
+# Testing model
+patiente = X_test.iloc[[0]]
+
+prediction = model_xgb.predict_proba(patiente)
+prediction_final = model_xgb.predict(patiente)
+print(f'Prediction with saved model => {prediction}')
+print(f'Prediction probability with saved model => {prediction_final[0]}')
+
+print(f'Probability of Status_C => {prediction[0][1]}')
+print(f'Probability of Status_CL => {prediction[0][2]}')
+print(f'Probability of Status_D => {prediction[0][0]}')
