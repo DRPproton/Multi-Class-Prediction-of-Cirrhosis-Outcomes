573f4f180ab7b4c7421c87ef2437147b0686b059
DRPproton <86702004+DRPproton@users.noreply.github.com>
2023-12-10 13:52:09 -0600

    preprocesiing day columns done

--- train.py
+++ train.py
@@ -17,5 +17,40 @@ import warnings
 warnings.filterwarnings("ignore")
 
 # Importing the datasets
+print('Importing datasets...')
 df_train = pd.read_csv('data/train.csv')   # Syntetic data
 df_original = pd.read_csv('data/cirrhosis.csv')  # original dataset
+time.sleep(1)
+
+
+# Preparing datasets
+df = pd.concat([df_train, df_original], axis =0)
+# drop the "id" column
+df.drop(columns=['id', 'ID'], inplace=True)
+df.reset_index(drop=True, inplace=True)
+# Dropping ows with null values
+df = df.dropna()
+
+
+# Function to process days columns
+def process_day_columns(input_df):
+    from datetime import datetime
+    # Define the starting date
+    df = input_df.copy()
+    start_date = datetime(1986, 7, 1)
+
+    # Convert days to months and years using pandas
+    df['date'] = start_date + pd.to_timedelta(df['N_Days'], unit='D')
+    df['N_Months'] = (df['date'].dt.year - start_date.year) * 12 + df['date'].dt.month - start_date.month
+    # Age column to year
+    df['Age'] = df['Age'] // 365.25 
+
+    df.drop(columns='date', inplace=True)
+
+    return df
+
+df_proc = process_day_columns(df)
+
+print(df_proc.head())
+
+
