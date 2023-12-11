2146d5b8d4a0d2773ade8c75aa17963615129c8c
DRPproton <86702004+DRPproton@users.noreply.github.com>
2023-12-10 21:00:31 -0600

    train file done

--- train.py
+++ train.py
@@ -6,13 +6,14 @@ Dashel Ruiz Perez 12/10/2023
 
 # Importig libraries
 import pandas as pd
-import numpy as np
+import pickle
 import time
 from sklearn.metrics import roc_auc_score, log_loss
 from sklearn.model_selection import train_test_split
 from xgboost import XGBClassifier
 from sklearn.preprocessing import RobustScaler
-from sklearn.preprocessing import OrdinalEncoder
+from sklearn.feature_extraction import DictVectorizer
+from utils import *
 
 # Supressing warnigns
 import warnings
@@ -34,102 +35,58 @@ df.reset_index(drop=True, inplace=True)
 # Dropping ows with null values
 df = df.dropna()
 
-
-# Function to process days columns
-def process_day_columns(input_df):
-    from datetime import datetime
-    # Define the starting date
-    df = input_df.copy()
-    start_date = datetime(1986, 7, 1)
-
-    # Convert days to months and years using pandas
-    df['date'] = start_date + pd.to_timedelta(df['N_Days'], unit='D')
-    df['N_Months'] = (df['date'].dt.year - start_date.year) * 12 + df['date'].dt.month - start_date.month
-    # Age column to year
-    df['Age'] = df['Age'] // 365.25 
-
-    df.drop(columns='date', inplace=True)
-
-    return df
-
+# Processing day columns
 df_proc = process_day_columns(df)
 
 # Converting 'Stage' to category
 df_proc.Stage = df_proc.Stage.astype('category')
 
-
-# Function to make new features
-def feature_eng_num_to_cat(df):
-    df_temp = df.copy()
-    df_temp['normal_patelets'] = df_temp.Platelets.map(lambda x: "N" if x < 150 else "Y") # Pateletes
-    df_temp['normal_cholesterol'] = df_temp.Cholesterol.map(lambda x: "Y" if x < 201 else "N") # Cholesterol
-    df_temp['normal_tryglicerides'] = df_temp.Tryglicerides.map(lambda x: "Y" if x < 151 else "N") # Tryglicerides
-    df_temp['normal_SGOT'] = df_temp.SGOT.map(lambda x: "Y" if x < 41 else "N") # SGOT
-    df_temp['normal_copper'] = df_temp.Copper.map(lambda x: "Y" if x >= 62 and x <= 140 else "N") # Copper
-    df_temp['normal_bilirubin'] = df_temp.Bilirubin.map(lambda x: "Y" if x >= 0.2 and x <= 1.2 else "N") # Bilirubin
-    df_temp['normal_albumin'] = df_temp.Albumin.map(lambda x: "Y" if x >= 3.4 and x <= 5.4 else "N") # Albumin
-
-    return df_temp
-
+# Feature eng
 df_new_features = feature_eng_num_to_cat(df_proc)
 
 # Converting target to numeric
 df_new_features.Status = df_new_features.Status.map({"D": 0, "C": 1, "CL": 2})
 
 
-cols_to_encode = ['Drug', 'Sex', 'Ascites', 'Hepatomegaly', 'Spiders', 'Stage', 
-                'normal_patelets', 'normal_cholesterol', 'normal_tryglicerides', 
-                'normal_SGOT', 'normal_copper', 'normal_bilirubin', 'normal_albumin']
-# Making the encoder
-encoder = OrdinalEncoder()
-
-# Function with encode categorical variable
-def encode_cols(df, bi_cols, dummy_nom_list, encoder):
-    df_temp = df.copy()
-    df_temp[bi_cols] = encoder.fit_transform(df_temp[bi_cols])
-
-    for col in  dummy_nom_list:
-        df_temp = pd.concat([df_temp.drop(col, axis = 1),
-                            pd.get_dummies(df_temp[col], prefix = col, prefix_sep = '_', drop_first = True, dummy_na = False, dtype='int32')],
-                        axis=1)
-
-    return df_temp
-
-df_final = encode_cols(df_new_features, cols_to_encode, ['Edema'],encoder)
-
-# Split the data and Standarize
-X = df_final.drop(columns='Status')
-y = df_final.Status
+# Spliting the data
+X = df_new_features.drop(columns='Status')
+y = df_new_features.Status
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=123, test_size=0.2, stratify=y)
 
+# DictVectorizer
+dv = DictVectorizer(sparse=False)
 
+train_dict = X_train.to_dict(orient='records')
+test_dict = X_test.to_dict(orient='records')
+
+X_train = dv.fit_transform(train_dict)
+X_test = dv.transform(test_dict)
+
+# Scaler
 sc = RobustScaler()
-sc_df = sc.fit_transform(X_train)
-
-X_train = pd.DataFrame(sc_df, columns=X_train.columns)
-X_test = pd.DataFrame(sc.transform(X_test), columns=X_test.columns)
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
 
 
-print('Creating model...')
-time.sleep(2)
-# XGB model , params got from optuna tuning
-best_parameters = {'booster': 'gbtree', 
-                   'max_depth': 11, 
-                   'learning_rate': 0.04216709720284281, 
-                   'n_estimators': 504, 
-                   'min_child_weight': 1, 
-                   'subsample': 0.3437341948452076, 
-                   'colsample_bylevel': 0.9831950290382536, 
-                   'colsample_bytree': 0.1720049167961935, 
-                   'colsample_bynode': 0.7420031757532206, 
-                   'reg_alpha': 0.9242740053249385, 
-                   'reg_lambda': 0.9419384081768526, 
-                   'eval_metric': 'mlogloss'}
+# Model
+best_params = {'booster': 'gbtree', 
+               'max_depth': 11, 
+               'learning_rate': 0.04216709720284281, 
+               'n_estimators': 504, 
+               'min_child_weight': 1, 
+               'subsample': 0.3437341948452076, 
+               'colsample_bylevel': 0.9831950290382536, 
+               'colsample_bytree': 0.1720049167961935, 
+               'colsample_bynode': 0.7420031757532206, 
+               'reg_alpha': 0.9242740053249385, 
+               'reg_lambda': 0.9419384081768526, 
+               'eval_metric': 'mlogloss'}
 
-xgb = XGBClassifier(**best_parameters)
+xgb = XGBClassifier(**best_params)
 xgb.fit(X_train, y_train)
 xgb_pred = xgb.predict_proba(X_test)
+
 
 print('Making prediciions...')
 print(f'Logloss score => {log_loss(y_test, xgb_pred)}')
@@ -140,25 +97,68 @@ time.sleep(2)
 
 # Exporting model
 print('Exporting model...')
-import pickle
-
 with open('xgb_model.bin', 'wb') as f_out:
     pickle.dump(xgb, f_out)
 
+with open('dv_model.bin', 'wb') as f_out:
+    pickle.dump(dv, f_out)
+
+with open('scaler.bin', 'wb') as f_out:
+    pickle.dump(sc, f_out)
+
+# Imprting models for testing
 time.sleep(1)
 print('Importing saved model...')
 with open('xgb_model.bin', 'rb') as f_in:
-    model_xgb = pickle.load(f_in)
+    xgb_model = pickle.load(f_in)
+
+with open('dv_model.bin', 'rb') as f_in:
+    dv_model = pickle.load(f_in)
+
+with open('scaler.bin', 'rb') as f_in:
+    scaler_model = pickle.load(f_in)
 
 
 # Testing model
-patiente = X_test.iloc[[0]]
+patient = {
+ 'N_Days': 3839,
+ 'Drug': 'D-penicillamine',
+ 'Age': 19724,
+ 'Sex': 'F',
+ 'Ascites': 'N',
+ 'Hepatomegaly': 'Y',
+ 'Spiders': 'N',
+ 'Edema': 'N',
+ 'Bilirubin': 1.2,
+ 'Cholesterol': 546.0,
+ 'Albumin': 3.37,
+ 'Copper': 65.0,
+ 'Alk_Phos': 1636.0,
+ 'SGOT': 151.9,
+ 'Tryglicerides': 90.0,
+ 'Platelets': 430.0,
+ 'Prothrombin': 10.6,
+ 'Stage': 2.0}
 
-prediction = model_xgb.predict_proba(patiente)
-prediction_final = model_xgb.predict(patiente)
-print(f'Prediction with saved model => {prediction}')
-print(f'Prediction probability with saved model => {prediction_final[0]}')
+patiente_df = pd.DataFrame([patient])
 
-print(f'Probability of Status_C => {prediction[0][1]}')
-print(f'Probability of Status_CL => {prediction[0][2]}')
-print(f'Probability of Status_D => {prediction[0][0]}')
+df_test = process_day_columns(patiente_df)
+df_test.Stage = df_test.Stage.astype('category')
+df_test = feature_eng_num_to_cat(df_test)
+
+df_test_dict = df_test.to_dict(orient='records')
+
+df_test = dv_model.transform(df_test_dict)
+df_test = scaler_model.transform(df_test)
+
+
+prediction = xgb_model.predict(df_test)
+proba = xgb_model.predict_proba(df_test)
+
+
+print(f'Prediction with saved model => {prediction[0]}')
+print(f'Prediction probability with saved model => {proba}')
+
+print(f'Probability of Status_C => {proba[0][1]}')
+print(f'Probability of Status_CL => {proba[0][2]}')
+print(f'Probability of Status_D => {proba[0][0]}')
