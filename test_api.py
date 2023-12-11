a00b6eecdc54591cfa2da8c90f5b3b514d6d1f93
DRPproton <86702004+DRPproton@users.noreply.github.com>
2023-12-11 12:07:12 -0600

    Changes made to the testapi file. Everything working

--- test_api.py
+++ test_api.py
@@ -3,13 +3,13 @@ import requests
 patient = {
  'N_Days': 3839,
  'Drug': 'D-penicillamine',
- 'Age': 19724,
+ 'Age': 29724,
  'Sex': 'F',
  'Ascites': 'N',
  'Hepatomegaly': 'Y',
  'Spiders': 'N',
  'Edema': 'N',
- 'Bilirubin': 1.2,
+ 'Bilirubin': 2.2,
  'Cholesterol': 546.0,
  'Albumin': 3.37,
  'Copper': 65.0,
@@ -18,7 +18,7 @@ patient = {
  'Tryglicerides': 90.0,
  'Platelets': 430.0,
  'Prothrombin': 10.6,
- 'Stage': 2.0}
+ 'Stage': 3.0}
 
 url = "http://localhost:9696/predict"
 
