291494d4c26e13ce17775929917ac48723d6bb31
DRPproton <86702004+DRPproton@users.noreply.github.com>
2023-12-10 21:05:22 -0600

    Adding test api file

---
+++ test_api.py
@@ -0,0 +1,27 @@
+import requests
+
+patient = {
+ 'N_Days': 3839,
+ 'Drug': 'D-penicillamine',
+ 'Age': 19724,
+ 'Sex': 'F',
+ 'Ascites': 'N',
+ 'Hepatomegaly': 'Y',
+ 'Spiders': 'N',
+ 'Edema': 'N',
+ 'Bilirubin': 1.2,
+ 'Cholesterol': 546.0,
+ 'Albumin': 3.37,
+ 'Copper': 65.0,
+ 'Alk_Phos': 1636.0,
+ 'SGOT': 151.9,
+ 'Tryglicerides': 90.0,
+ 'Platelets': 430.0,
+ 'Prothrombin': 10.6,
+ 'Stage': 2.0}
+
+url = "http://localhost:9696/predict"
+
+response = requests.post(url, json=patient).json()
+
+print(response)
